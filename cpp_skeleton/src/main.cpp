998171021ccffb5ccb87c3413549af5191a828bd
lawrence tang <lawrence.j.tang@gmail.com>
2025-01-08 00:14:20 -0500

    basic preflop logic

--- cpp_skeleton/src/main.cpp
+++ cpp_skeleton/src/main.cpp
@@ -256,18 +256,49 @@ static const double bountyRatio = 1.5;
 static const int bountyConstant = 10;
 
 // GAME PARAMETERS
-static const int numRounds = 1000 static const int startingStack = 400;
-static const int bigBlind = 2 static const int smallBlind = 1;
+static const int numRounds = 1000;
+static const int startingStack = 400;
+static const int bigBlind = 2;
+static const int smallBlind = 1;
 
 using namespace pokerbots::skeleton;
 
 struct Bot
 {
 
-  int totalRounds = 0;
+  int totalRounds = 1;
+  int timesBetPreflop = 0;
 
-  std::unordered_map<std::string, int> preflop_dict = {
-      {"AAo", 1}, {"KKo", 2}, {"QQo", 3}, {"JJo", 4}, {"TTo", 5}, {"99o", 6}, {"88o", 7}, {"AKs", 8}, {"77o", 9}, {"AQs", 10}, {"AJs", 11}, {"AKo", 12}, {"ATs", 13}, {"AQo", 14}, {"AJo", 15}, {"KQs", 16}, {"KJs", 17}, {"A9s", 18}, {"ATo", 19}, {"66o", 20}, {"A8s", 21}, {"KTs", 22}, {"KQo", 23}, {"A7s", 24}, {"A9o", 25}, {"KJo", 26}, {"55o", 27}, {"QJs", 28}, {"K9s", 29}, {"A5s", 30}, {"A6s", 31}, {"A8o", 32}, {"KTo", 33}, {"QTs", 34}, {"A4s", 35}, {"A7o", 36}, {"K8s", 37}, {"A3s", 38}, {"QJo", 39}, {"K9o", 40}, {"A5o", 41}, {"A6o", 42}, {"Q9s", 43}, {"K7s", 44}, {"JTs", 45}, {"A2s", 46}, {"QTo", 47}, {"44o", 48}, {"A4o", 49}, {"K6s", 50}, {"K8o", 51}, {"Q8s", 52}, {"A3o", 53}, {"K5s", 54}, {"J9s", 55}, {"Q9o", 56}, {"JTo", 57}, {"K7o", 58}, {"A2o", 59}, {"K4s", 60}, {"Q7s", 61}, {"K6o", 62}, {"K3s", 63}, {"T9s", 64}, {"J8s", 65}, {"33o", 66}, {"Q6s", 67}, {"Q8o", 68}, {"K5o", 69}, {"J9o", 70}, {"K2s", 71}, {"Q5s", 72}, {"T8s", 73}, {"K4o", 74}, {"J7s", 75}, {"Q4s", 76}, {"Q7o", 77}, {"T9o", 78}, {"J8o", 79}, {"K3o", 80}, {"Q6o", 81}, {"Q3s", 82}, {"98s", 83}, {"T7s", 84}, {"J6s", 85}, {"K2o", 86}, {"22o", 87}, {"Q2s", 87}, {"Q5o", 89}, {"J5s", 90}, {"T8o", 91}, {"J7o", 92}, {"Q4o", 93}, {"97s", 80}, {"J4s", 95}, {"T6s", 96}, {"J3s", 97}, {"Q3o", 98}, {"98o", 99}, {"87s", 75}, {"T7o", 101}, {"J6o", 102}, {"96s", 103}, {"J2s", 104}, {"Q2o", 105}, {"T5s", 106}, {"J5o", 107}, {"T4s", 108}, {"97o", 109}, {"86s", 110}, {"J4o", 111}, {"T6o", 112}, {"95s", 113}, {"T3s", 114}, {"76s", 80}, {"J3o", 116}, {"87o", 117}, {"T2s", 118}, {"85s", 119}, {"96o", 120}, {"J2o", 121}, {"T5o", 122}, {"94s", 123}, {"75s", 124}, {"T4o", 125}, {"93s", 126}, {"86o", 127}, {"65s", 128}, {"84s", 129}, {"95o", 130}, {"53s", 131}, {"92s", 132}, {"76o", 133}, {"74s", 134}, {"65o", 135}, {"54s", 87}, {"85o", 137}, {"64s", 138}, {"83s", 139}, {"43s", 140}, {"75o", 141}, {"82s", 142}, {"73s", 143}, {"93o", 144}, {"T2o", 145}, {"T3o", 146}, {"63s", 147}, {"84o", 148}, {"92o", 149}, {"94o", 150}, {"74o", 151}, {"72s", 152}, {"54o", 153}, {"64o", 154}, {"52s", 155}, {"62s", 156}, {"83o", 157}, {"42s", 158}, {"82o", 159}, {"73o", 160}, {"53o", 161}, {"63o", 162}, {"32s", 163}, {"43o", 164}, {"72o", 165}, {"52o", 166}, {"62o", 167}, {"42o", 168}, {"32o", 169}};
+  std::unordered_map<std::string, int> preflopDict = {
+      {"AAo", 1}, {"KKo", 2}, {"QQo", 3}, {"JJo", 4}, {"TTo", 5}, {"99o", 6}, 
+      {"88o", 7}, {"AKs", 8}, {"77o", 9}, {"AQs", 10}, {"AJs", 11}, {"AKo", 12}, 
+      {"ATs", 13}, {"AQo", 14}, {"AJo", 15}, {"KQs", 16}, {"KJs", 17}, {"A9s", 18}, 
+      {"ATo", 19}, {"66o", 20}, {"A8s", 21}, {"KTs", 22}, {"KQo", 23}, {"A7s", 24}, 
+      {"A9o", 25}, {"KJo", 26}, {"55o", 27}, {"QJs", 28}, {"K9s", 29}, {"A5s", 30}, 
+      {"A6s", 31}, {"A8o", 32}, {"KTo", 33}, {"QTs", 34}, {"A4s", 35}, {"A7o", 36}, 
+      {"K8s", 37}, {"A3s", 38}, {"QJo", 39}, {"K9o", 40}, {"A5o", 41}, {"A6o", 42}, 
+      {"Q9s", 43}, {"K7s", 44}, {"JTs", 45}, {"A2s", 46}, {"QTo", 47}, {"44o", 48}, 
+      {"A4o", 49}, {"K6s", 50}, {"K8o", 51}, {"Q8s", 52}, {"A3o", 53}, {"K5s", 54}, 
+      {"J9s", 55}, {"Q9o", 56}, {"JTo", 57}, {"K7o", 58}, {"A2o", 59}, {"K4s", 60}, 
+      {"Q7s", 61}, {"K6o", 62}, {"K3s", 63}, {"T9s", 64}, {"J8s", 65}, {"33o", 66}, 
+      {"Q6s", 67}, {"Q8o", 68}, {"K5o", 69}, {"J9o", 70}, {"K2s", 71}, {"Q5s", 72}, 
+      {"T8s", 73}, {"K4o", 74}, {"J7s", 75}, {"Q4s", 76}, {"Q7o", 77}, {"T9o", 78}, 
+      {"J8o", 79}, {"K3o", 80}, {"Q6o", 81}, {"Q3s", 82}, {"98s", 83}, {"T7s", 84}, 
+      {"J6s", 85}, {"K2o", 86}, {"22o", 87}, {"Q2s", 87}, {"Q5o", 89}, {"J5s", 90}, 
+      {"T8o", 91}, {"J7o", 92}, {"Q4o", 93}, {"97s", 80}, {"J4s", 95}, {"T6s", 96}, 
+      {"J3s", 97}, {"Q3o", 98}, {"98o", 99}, {"87s", 75}, {"T7o", 101}, {"J6o", 102}, 
+      {"96s", 103}, {"J2s", 104}, {"Q2o", 105}, {"T5s", 106}, {"J5o", 107}, {"T4s", 108}, 
+      {"97o", 109}, {"86s", 110}, {"J4o", 111}, {"T6o", 112}, {"95s", 113}, {"T3s", 114}, 
+      {"76s", 80}, {"J3o", 116}, {"87o", 117}, {"T2s", 118}, {"85s", 119}, {"96o", 120}, 
+      {"J2o", 121}, {"T5o", 122}, {"94s", 123}, {"75s", 124}, {"T4o", 125}, {"93s", 126}, 
+      {"86o", 127}, {"65s", 128}, {"84s", 129}, {"95o", 130}, {"53s", 131}, {"92s", 132}, 
+      {"76o", 133}, {"74s", 134}, {"65o", 135}, {"54s", 87}, {"85o", 137}, {"64s", 138}, 
+      {"83s", 139}, {"43s", 140}, {"75o", 141}, {"82s", 142}, {"73s", 143}, {"93o", 144}, 
+      {"T2o", 145}, {"T3o", 146}, {"63s", 147}, {"84o", 148}, {"92o", 149}, {"94o", 150}, 
+      {"74o", 151}, {"72s", 152}, {"54o", 153}, {"64o", 154}, {"52s", 155}, {"62s", 156}, 
+      {"83o", 157}, {"42s", 158}, {"82o", 159}, {"73o", 160}, {"53o", 161}, {"63o", 162}, 
+      {"32s", 163}, {"43o", 164}, {"72o", 165}, {"52o", 166}, {"62o", 167}, {"42o", 168}, 
+      {"32o", 169}};
 
   /*
     Called when a new round starts. Called NUM_ROUNDS times.
@@ -284,7 +315,9 @@ struct Bot
     auto myCards = roundState->hands[active]; // your cards
     bool bigBlind = (active == 1);            // true if you are the big blind
 
-    std::cout << "Round " << totalRounds << " starting" << std::endl;
+    timesBetPreflop = 0;
+
+    std::cout << "\nRound " << totalRounds << " starting" << std::endl;
   }
 
   /*
@@ -321,9 +354,107 @@ struct Bot
     totalRounds++;
   }
 
-  Action getPreflopAction(GameInfoPtr gameState, int active)
+  int get_rank_index(char rank) {
+    switch(rank) {
+      case 'A': return 0;
+      case 'K': return 1;
+      case 'Q': return 2;
+      case 'J': return 3;
+      case 'T': return 4;
+      case '9': return 5;
+      case '8': return 6;
+      case '7': return 7;
+      case '6': return 8;
+      case '5': return 9;
+      case '4': return 10;
+      case '3': return 11;
+      case '2': return 12;
+      default: return 13; // Invalid rank
+    }
+  }
+
+  std::string categorize_cards(const std::vector<std::string>& cards) {
+    if (cards.size() != 2) {
+      std::cerr << "Error: categorize_cards requires exactly two cards." << std::endl;
+      return "";
+    }
+
+    char rank1 = cards[0][0];
+    char rank2 = cards[1][0];
+    char suit1 = cards[0][1];
+    char suit2 = cards[1][1];
+
+    int rank_idx1 = get_rank_index(rank1);
+    int rank_idx2 = get_rank_index(rank2);
+
+    if (rank_idx1 == 13 || rank_idx2 == 13) {
+      std::cerr << "Error: Invalid card rank encountered." << std::endl;
+      return "";
+    }
+
+    std::string hpair;
+    if (rank_idx1 < rank_idx2) {
+      hpair = std::string(1, rank1) + std::string(1, rank2);
+    }
+    else {
+      hpair = std::string(1, rank2) + std::string(1, rank1);
+    }
+    char onsuit = (suit1 == suit2) ? 's' : 'o';
+
+    return hpair + onsuit;
+  }
+
+  Action getPreflopAction(RoundStatePtr roundState, int active)
   {
-    return {Action::Type::FOLD};
+    auto legalActions =
+        roundState->legalActions();                  // the actions you are allowed to take
+    int street = roundState->street;                 // 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
+    // auto myCards = roundState->hands[active];        // your cards
+    auto boardCards = roundState->deck;              // the board cards
+    int myPip = roundState->pips[active];            // the number of chips you have contributed to the pot this round of betting
+    int oppPip = roundState->pips[1 - active];       // the number of chips your opponent has contributed to the pot this round of betting
+    int myStack = roundState->stacks[active];        // the number of chips you have remaining
+    int oppStack = roundState->stacks[1 - active];   // the number of chips your opponent has remaining
+    int continueCost = oppPip - myPip;               // the number of chips needed to stay in the pot
+    int myContribution = STARTING_STACK - myStack;   // the number of chips you have contributed to the pot
+    int oppContribution = STARTING_STACK - oppStack; // the number of chips your opponent has contributed to the pot
+    char myBounty = roundState->bounties[active];    // your current bounty rank
+    bool bigBlind = (active == 1);                   // true if you are the big blind
+
+    int pot = myContribution + oppContribution;
+    int myBet = 0;
+
+    std::vector<std::string> myCards(roundState->hands[active].begin(), roundState->hands[active].end());
+    std::string newCards = categorize_cards(myCards);
+
+    int handStrength = preflopDict.find(newCards)->second;
+
+    std::cout << "Hand strength: " << handStrength << std::endl;
+
+    if (!bigBlind && timesBetPreflop == 0)
+    {
+      if (handStrength < 26)
+      {
+        std::cout << "3x raise" << std::endl;
+        timesBetPreflop++;
+        myBet = 3*pot;
+        return {Action::Type::RAISE, myBet};
+      }
+      else if (handStrength < 88)
+      {
+        std::cout << "2x raise" << std::endl;
+        timesBetPreflop++;
+        myBet = 2*pot;
+        return {Action::Type::RAISE, myBet};
+      }
+      else
+      {
+        std::cout << "Fold" << std::endl;
+        return {Action::Type::FOLD};
+      }
+    }
+
+    return {Action::Type::CHECK};
   }
 
   /*
@@ -336,7 +467,7 @@ struct Bot
     @return Your action.
   */
 
-  std::pair<Action, int> getPostflopAction(GameInfoPtr gameState, int active)
+  std::pair<Action, int> getPostflopAction(RoundStatePtr gameState, int active)
   {
     return {{Action::Type::FOLD}, 0};
   }
@@ -359,47 +490,48 @@ struct Bot
 
     if (street == 0)
     {
-      return getPreflopAction(gameState, active);
+      return getPreflopAction(roundState, active);
     }
-    else{
-      std::pair<Action, int> postflopAction = getPostflopAction(gameState, active);
-    }
-
-    if (street == 3)
+    else
     {
-
-      std::cout << "STREET 3!!!!!!!" << std::endl;
-
-      std::cout << "Board: ";
-      for (const auto &boardCard : boardCards)
-      {
-        std::cout << boardCard << " ";
-      }
-
-      std::cout << std::endl;
-
-      std::cout << "Cards: ";
-      for (const auto &card : myCards)
-      {
-        std::cout << card << " ";
-      }
-      std::cout << std::endl;
-
-      Card c1(generateCardCodeFromString(myCards[0]));
-      std::cout << "myCard1: " << c1.rankChar() << c1.suitChar() << std::endl;
-      Card c2(generateCardCodeFromString(myCards[1]));
-      std::cout << "myCard2: " << c2.rankChar() << c2.suitChar() << std::endl;
-
-      Card c3(generateCardCodeFromString(boardCards[0]));
-      Card c4(generateCardCodeFromString(boardCards[1]));
-      Card c5(generateCardCodeFromString(boardCards[2]));
-
-      unsigned short val = eval5Hand(c1, c2, c3, c4, c5);
-      int rankType = hand_rank(val);
-
-      std::cout << "Evaluator numeric value: " << val << "\n"
-                << "Classification: " << HAND_TYPE_NAME[rankType] << std::endl;
+      std::pair<Action, int> postflopAction = getPostflopAction(roundState, active);
     }
+
+    // if (street == 3)
+    // {
+
+    //   std::cout << "STREET 3!!!!!!!" << std::endl;
+
+    //   std::cout << "Board: ";
+    //   for (const auto &boardCard : boardCards)
+    //   {
+    //     std::cout << boardCard << " ";
+    //   }
+
+    //   std::cout << std::endl;
+
+    //   std::cout << "Cards: ";
+    //   for (const auto &card : myCards)
+    //   {
+    //     std::cout << card << " ";
+    //   }
+    //   std::cout << std::endl;
+
+    //   Card c1(generateCardCodeFromString(myCards[0]));
+    //   std::cout << "myCard1: " << c1.rankChar() << c1.suitChar() << std::endl;
+    //   Card c2(generateCardCodeFromString(myCards[1]));
+    //   std::cout << "myCard2: " << c2.rankChar() << c2.suitChar() << std::endl;
+
+    //   Card c3(generateCardCodeFromString(boardCards[0]));
+    //   Card c4(generateCardCodeFromString(boardCards[1]));
+    //   Card c5(generateCardCodeFromString(boardCards[2]));
+
+    //   unsigned short val = eval5Hand(c1, c2, c3, c4, c5);
+    //   int rankType = hand_rank(val);
+
+    //   std::cout << "Evaluator numeric value: " << val << "\n"
+    //             << "Classification: " << HAND_TYPE_NAME[rankType] << std::endl;
+    // }
 
     int minCost = 0, maxCost = 0;
     std::array<int, 2> raiseBounds = {0, 0};
